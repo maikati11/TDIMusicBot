8e49db001754ec938af0d33dff0f434455ddc1dc
maikati11 <136818770+maikati11@users.noreply.github.com>
2024-10-24 22:55:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const DisTube = require('distube');
 const connectDB = require('./database');
 const PlayerManager = require('./player/PlayerManager');
 const { printWatermark } = require('./config/type.js');
+require('dotenv').config(); // Зарежда .env файла, ако съществува
+
+// Инициализиране на Discord клиента
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -14,42 +17,57 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
   ],
 });
+
+// Свързване към базата данни
 connectDB();
 printWatermark();
+
+// Колекции за команди
 client.commands = new Collection();
 client.playerManager = new PlayerManager(client, distubeOptions);
 client.playerManager.distube.setMaxListeners(20);
+
+// Зареждане на команди
 const commandsPath = path.join(__dirname, './commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
   const command = require(path.join(commandsPath, file));
-  client.commands.set(command.data.name, command);
+  if (command && command.data && command.data.name) { // Проверка за валидни команди
+    client.commands.set(command.data.name, command);
+  }
 }
 
-
+// Зареждане на събития
 const eventsPath = path.join(__dirname, './events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
   const filePath = path.join(eventsPath, file);
   const event = require(filePath);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args, client));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args, client));
+  if (event && event.name) { // Проверка за валидни събития
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+      client.on(event.name, (...args) => event.execute(...args, client));
+    }
   }
 }
 
+// Express сървър за уеб интерфейс
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Използвай променлива за порта
 app.get('/', (req, res) => {
     const imagePath = path.join(__dirname, 'index.html');
     res.sendFile(imagePath);
 });
+
 app.listen(port, () => {
     console.log(`🔗 Listening to GlaceYT : http://localhost:${port}`);
 });
 
-client.login(process.env.TOKEN);
+// Стартиране на Discord бота
+client.login(process.env.TOKEN).catch(err => {
+  console.error("Failed to login: ", err);
+});
